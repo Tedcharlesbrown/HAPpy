d1cce13baaf62d710872b86b0eede8e417c52559
Ted Charles Brown <52015952+Tedcharlesbrown@users.noreply.github.com>
2023-10-09 20:48:27 -0400

    asdl;fkjmasd;lf

--- src_ffmpeg/encode.py
+++ src_ffmpeg/encode.py
@@ -1,5 +1,7 @@
 import ffmpeg
 import os
+import subprocess
+import re
 
 # Adjust these paths based on where you're keeping ffmpeg and ffprobe relative to your script
 current_directory = os.path.dirname(os.path.abspath(__file__))
@@ -10,12 +12,85 @@ ffprobe_bin = os.path.join(current_directory, 'FFMPEG')  # or 'ffmpeg/bin/ffprob
 ffmpeg._run.DEFAULT_FFMPEG_PATH = ffmpeg_bin
 ffmpeg._run.DEFAULT_FFPROBE_PATH = ffprobe_bin
 
-def encode_to_hap(input_path, output_path):
-    # Note: the exact FFmpeg command might vary based on the specific FFmpeg build and HAP variant.
-    # The example below is for HAP encoding
+# def encode_to_hap(input_path, output_path):
+#     if not os.path.exists(os.path.dirname(output_path)):
+#         os.makedirs(os.path.dirname(output_path))
+#     stream = ffmpeg.input(input_path)
+#     stream = ffmpeg.output(stream, output_path + ".mov", vcodec='hap', format='mov', compressor='snappy')
+#     ffmpeg.run(stream)
+
+def get_video_dimensions(input_path):
+    probe = ffmpeg.probe(input_path)
+    video_stream = next((stream for stream in probe['streams'] if stream['codec_type'] == 'video'), None)
+    return int(video_stream['width']), int(video_stream['height'])
+
+def round_up_to_nearest_four(n):
+    return (n + 3) & ~3  # Rounds up to the nearest multiple of 4
+
+def encode_to_hap(input_path, output_path, mode="pad", callback=None):
+    width, height = get_video_dimensions(input_path)
+
+    new_width = round_up_to_nearest_four(width)
+    new_height = round_up_to_nearest_four(height)
+
+    if not os.path.exists(os.path.dirname(output_path)):
+        os.makedirs(os.path.dirname(output_path))
+
     stream = ffmpeg.input(input_path)
-    stream = ffmpeg.output(stream, output_path, vcodec='hap', format='mov', compressor='snappy')
-    ffmpeg.run(stream)
+
+    if mode == "pad":
+        pad_right = new_width - width
+        pad_bottom = new_height - height
+        stream = ffmpeg.filter(stream, 'pad', new_width, new_height, 0, 0, color="black")
+    elif mode == "scale":
+        stream = ffmpeg.filter(stream, 'scale', new_width, new_height)
+    else:
+        raise ValueError("Invalid mode. Please use 'pad' or 'scale'.")
+
+    stream = ffmpeg.output(stream, output_path + ".mov", vcodec='hap', format='mov', compressor='snappy')
+    
+    cmd = ffmpeg.compile(stream)
+    process = subprocess.Popen(cmd, stderr=subprocess.PIPE, universal_newlines=True)
+
+    duration_pattern = re.compile(r"Duration: (\d+:\d+:\d+\.\d+)")
+    progress_pattern = re.compile(r"time=(\d+:\d+:\d+\.\d+)")
+
+    duration = None
+    for line in iter(process.stderr.readline, ""):
+        if duration is None:
+            match = duration_pattern.search(line)
+            if match:
+                duration = sum(float(x) * 60 ** i for i, x in enumerate(reversed(match.group(1).split(":"))))
+        match = progress_pattern.search(line)
+        if match:
+            progress_time = sum(float(x) * 60 ** i for i, x in enumerate(reversed(match.group(1).split(":"))))
+            progress_percentage = (progress_time / duration) * 100 if duration else 0
+            if callback:
+                callback(progress_percentage)
+
+    return_code = process.wait()
+    # After FFmpeg finishes, make sure you set the progress to 100%.
+    if callback:
+        callback(100.0)
+
+    if return_code != 0:
+        raise subprocess.CalledProcessError(return_code, cmd)
+    
+    return True
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 def encode_to_hap_half(input_path, output_path):
     # The example below is for HAP Q encoding with half resolution
--- src_tkinter/gui.py
+++ src_tkinter/gui.py
@@ -31,23 +31,39 @@ def display_file_tree(folder_path):
     """Displays the file tree of the specified folder in the Treeview."""
     for i in tree.get_children():
         tree.delete(i)
+    populate_tree("", folder_path)
 
-    parent_item = tree.insert("", "end", text=folder_path, open=True)
-    populate_tree(parent_item, folder_path)
+
+def populate_tree(parent, folder_path):
+    """Populates the Treeview with the file structure."""
+    if os.path.isdir(folder_path):
+        for entry in os.listdir(folder_path):
+            entry_path = os.path.join(folder_path, entry)
+            if os.path.isdir(entry_path):  # If it's a sub-directory
+                child_item = tree.insert(parent, "end", text=entry)  # Create a parent node for sub-directory
+                populate_tree(child_item, entry_path)  # Recurse into the sub-directory
+            else:  # If it's a file
+                filename = os.path.basename(entry_path)
+                tree.insert(parent, "end", text=filename, values=(entry_path,))  # Add as child node to current directory's node
+    else:  # If it's a single file
+        filename = os.path.basename(folder_path)
+        tree.insert(parent, "end", text=filename, values=(folder_path,))   
 
 def display_destination_folder(folder_path):
     """Displays the selected destination folder."""
     destination_label.config(text=folder_path)
 
-def populate_tree(parent, folder_path):
-    """Recursively populates the Treeview with the file structure."""
-    for entry in os.listdir(folder_path):
-        entry_path = os.path.join(folder_path, entry)
-        if os.path.isdir(entry_path):
-            child_item = tree.insert(parent, "end", text=entry)
-            populate_tree(child_item, entry_path)
-        else:
-            tree.insert(parent, "end", text=entry)
+def on_encode_click():
+    selected_items = tree.selection()
+    # If nothing is selected, select all items
+    if not selected_items:
+        selected_items = tree.get_children()
+
+    for item in selected_items:
+        print(tree.item(item)["text"])
+        pass
+
+# ---------------------------------- styling --------------------------------- #
 
 root = TkinterDnD.Tk()
 root.title("HAP.py")
@@ -88,13 +104,21 @@ style.map("TCombobox",
           selectbackground=[('readonly', "#555555")],
           selectforeground=[('readonly', "#555555")])
 
+# Configure style for the button
+style = ttk.Style(root)
+style.configure("Dark.TButton", background="#555555", foreground="#FFFFFF", font=("Arial", 14), relief="raised", padding=(20, 10))
+style.map("Dark.TButton", background=[('pressed', '#333333'), ('active', '#444444')])
+
+# ----------------------------------- input ---------------------------------- #
 
 # Source Drag and Drop Area
-drop_area = tk.Label(root, bg='#555555', fg='#FFFFFF', relief="raised", text="Drop a source folder here")
+drop_area = tk.Label(root, bg='#555555', fg='#FFFFFF', relief="raised", text="Click here to select a source file / folder")
 drop_area.place(x=20, y=80, width=350, height=60)
 drop_area.drop_target_register(DND_FILES)
 drop_area.dnd_bind('<<Drop>>', drop)
 drop_area.bind('<Button-1>', open_folder_dialog)
+
+# ----------------------------------- tree ----------------------------------- #
 
 # Treeview for displaying the file structure of the source
 tree = ttk.Treeview(root)
@@ -102,27 +126,29 @@ tree.place(x=20, y=150, width=350, height=300)  # Setting an absolute width and 
 tree['show'] = 'tree'
 
 # Register Treeview as drop target and bind drop event
-
 tree.drop_target_register(DND_FILES)
 tree.dnd_bind('<<Drop>>', drop)
 
 # Insert default values
-parent = tree.insert("", "end", text="No files selected")
+parent = tree.insert("", "end", text="Drop source file / folder here")
+
+# -------------------------------- destination ------------------------------- #
 
 # Destination Drag and Drop Area
-destination_drop_area = tk.Label(root, bg='#555555', fg='#FFFFFF', relief="raised", text="Drop a destination folder here")
+destination_drop_area = tk.Label(root, bg='#555555', fg='#FFFFFF', relief="raised", text="Click here to select a destination")
 destination_drop_area.place(x=20, y=470, width=350, height=60)
 destination_drop_area.drop_target_register(DND_FILES)
 destination_drop_area.dnd_bind('<<Drop>>', drop)
 destination_drop_area.bind('<Button-1>', open_folder_dialog)
 
 # Frame to display the selected destination folder
-destination_label = ttk.Label(root, text="No destination selected", wraplength=300, style="Destination.TLabel")
+destination_label = ttk.Label(root, text="Drop destination folder here", wraplength=300, style="Destination.TLabel")
 destination_label.place(x=20, y=540, width=350, height=40)
 destination_label.drop_target_register(DND_FILES)
 destination_label.dnd_bind('<<Drop>>', drop)
 destination_label.bind('<Button-1>', open_folder_dialog)
 
+# -------------------------------- checkboxes -------------------------------- #
 
 # IntVar to hold the state of the checkbutton (1 for checked, 0 for unchecked)
 var_destination_match_source = tk.IntVar()
@@ -133,25 +159,26 @@ var_create_thumbnails = tk.IntVar()
 var_create_thumbnails_only = tk.IntVar()
 var_advanced_options = tk.IntVar()
 
-# Create the checkbutton
-btn_destination_match_source = ttk.Checkbutton(root, text="Destination same as Source", variable=var_destination_match_source)
-btn_destination_match_source.place(x=450, y=80)
+checkbox_destination_match_source = ttk.Checkbutton(root, text="Destination same as Source", variable=var_destination_match_source)
+checkbox_destination_match_source.place(x=450, y=80)
 
-btn_append_hap = ttk.Checkbutton(root, text="Append HAP to filename (preserves version)", variable=var_append_hap)
-btn_append_hap.place(x=450, y=120)
+checkbox_append_hap = ttk.Checkbutton(root, text="Append HAP to filename (preserves version)", variable=var_append_hap)
+checkbox_append_hap.place(x=450, y=120)
 
-btn_create_proxys = ttk.Checkbutton(root, text="Create proxys", variable=var_create_proxys)
-btn_create_proxys.place(x=450, y=160)
-btn_create_proxys_only = ttk.Checkbutton(root, text="Create proxys only", variable=var_create_proxys_only)
-btn_create_proxys_only.place(x=470, y=180)
+checkbox_create_proxys = ttk.Checkbutton(root, text="Create proxys", variable=var_create_proxys)
+checkbox_create_proxys.place(x=450, y=160)
+checkbox_create_proxys_only = ttk.Checkbutton(root, text="Create proxys only", variable=var_create_proxys_only)
+checkbox_create_proxys_only.place(x=470, y=180)
 
-btn_create_thumbnails = ttk.Checkbutton(root, text="Create thumbnails", variable=var_create_thumbnails)
-btn_create_thumbnails.place(x=450, y=220)
-btn_create_thumbnails_only = ttk.Checkbutton(root, text="Create thumbnails only", variable=var_create_thumbnails_only)
-btn_create_thumbnails_only.place(x=470, y=240)
+checkbox_create_thumbnails = ttk.Checkbutton(root, text="Create thumbnails", variable=var_create_thumbnails)
+checkbox_create_thumbnails.place(x=450, y=220)
+checkbox_create_thumbnails_only = ttk.Checkbutton(root, text="Create thumbnails only", variable=var_create_thumbnails_only)
+checkbox_create_thumbnails_only.place(x=470, y=240)
 
-btn_advanced_options = ttk.Checkbutton(root, text="Advanced Codec Options", variable=var_advanced_options)
-btn_advanced_options.place(x=450, y=300)
+checkbox_advanced_options = ttk.Checkbutton(root, text="Advanced Codec Options", variable=var_advanced_options)
+checkbox_advanced_options.place(x=450, y=300)
+
+# --------------------------------- dropdown --------------------------------- #
 
 options = ["HAP", "HAP Alpha", "HAP Q", "HAP Q Alpha", "HAP Alpha Only"]
 codec_option = tk.StringVar()  # To store the selected option
@@ -161,8 +188,15 @@ dropdown['values'] = options  # Setting the options
 dropdown.current(1)  # Set the default value as the first option
 dropdown.place(x=470, y=325)
 
-# ----------------------------- state controller ----------------------------- #
-btn_create_proxys_only['state'] = 'disabled'
-btn_create_thumbnails_only['state'] = 'disabled'
+# ----------------------------- checkbox state controller ----------------------------- #
+checkbox_create_proxys_only['state'] = 'disabled'
+checkbox_create_thumbnails_only['state'] = 'disabled'
 
-root.mainloop()
+# ---------------------------------- encode ---------------------------------- #
+button_encode = ttk.Button(root, text="Encode", style="Dark.TButton", command=on_encode_click)
+button_encode.place(x=470, y=400)
+
+if __name__ == "__main__":
+    root.mainloop()
+
+    
