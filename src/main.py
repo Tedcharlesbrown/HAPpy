25de21450521bd29790fa36af269e9fec60f8436
Ted Charles Brown <52015952+Tedcharlesbrown@users.noreply.github.com>
2023-10-25 10:43:01 -0400

    class seperation

--- src/main.py
+++ src/main.py
@@ -11,22 +11,38 @@ VERSION = "0.0.1"
 #                                     MAIN                                     #
 # ---------------------------------------------------------------------------- #
 
-if __name__ == "__main__":
-    root = TkinterDnD.Tk()
-    root.title(APPNAME)
-    root.geometry("800x600")
-    root.resizable(False, False)
+# if __name__ == "__main__":
+#     root = TkinterDnD.Tk()
+#     root.title(APPNAME)
+#     root.geometry("800x600")
+#     root.resizable(False, False)
 
-    console = Logger(f".{APPNAME} Log.csv")
-    console.set_header("TIME,TYPE,MESSAGE")
-    app = ENCODER(root, console)
+#     console = Logger(f".{APPNAME} Log.csv")
+#     console.set_header("TIME,TYPE,MESSAGE")
+#     app = ENCODER(root, console)
 
-    result = console.create_log()
+#     result = console.create_log()
 
-    if not result:
-        console.log(app.encoder.get_version("ffmpeg"), "INIT")
-        console.log(app.encoder.get_version("ffprobe"), "INIT")
+#     if not result:
+#         console.log(app.encoder.get_version("ffmpeg"), "INIT")
+#         console.log(app.encoder.get_version("ffprobe"), "INIT")
 
-    console.log(f"{APPNAME} v{VERSION} started", "INIT")
+#     console.log(f"{APPNAME} v{VERSION} started", "INIT")
 
-    root.mainloop()
+#     root.mainloop()
+
+
+from HAPpy.gui_class import GUI
+
+
+root = TkinterDnD.Tk()
+root.title(APPNAME)
+root.geometry("800x600")
+root.resizable(False, False)
+
+gui = GUI(root)
+gui.setup_constants()
+gui.configure_styles()
+gui.setup_ui()
+
+root.mainloop()
