ad63800372d11c1a0b481e7682dac88a299401c0
Tedcharlesbrown <52015952+Tedcharlesbrown@users.noreply.github.com>
2023-11-05 15:13:17 -0500

    added dmg

--- Export/MAC_README.txt
+++
@@ -1,6 +0,0 @@
-RIGHT CLICK ON THE APP
-- Show Package Contents
-- Contents
-- MacOS
-
-Double click on HAPpy executable
--- src/autopytoexe_path.py
+++ src/autopytoexe_path.py
@@ -2,6 +2,9 @@ import sys
 import os
 
 def resource_path(relative_path, is_asset=True):
+    if sys.platform == 'darwin':
+        macos = True
+
     """ Get absolute path to resource, works for dev and for PyInstaller """
     if is_asset:
         relative_path = os.path.join("GUI/assets", relative_path)
@@ -10,8 +13,19 @@ def resource_path(relative_path, is_asset=True):
     
     try:
         base_path = sys._MEIPASS
-        return os.path.join(base_path, relative_path)
+        path = os.path.join(base_path, relative_path)
+        return path
     except Exception:
-        base_path = os.path.abspath(".")
         relative_path = os.path.relpath(relative_path)
-        return os.path.join(os.path.dirname(base_path), relative_path)
+
+        if macos:
+            base_path = os.path.abspath(".")
+            base_path = os.path.dirname(os.path.dirname(base_path))
+            base_path = os.path.join(base_path, "Contents/Resources")
+            path = os.path.join(base_path, relative_path)
+        else:
+            base_path = os.path.abspath(".")
+            path = os.path.join(os.path.dirname(base_path), relative_path)
+
+        print(f"RELATIVE PATH: {relative_path}, BASE PATH: {base_path}, PATH: {path}")
+        return path
